7405fe37d370af2be45c94a52b1ec2d89ead2339
Ehllay <max.pruther@gmail.com>
2023-10-01 17:05:34 +0100

    Partially implemented theme switcher

--- home/.config/hypr/scripts/wallpaper.sh
+++ home/.config/hypr/scripts/wallpaper.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-# Directory where your wallpapers are stored
-wallpaper_dir="$HOME/.config/hypr/wallpapers"
+# Directory where your wallpapers from the current theme are stored
+state_theme="$(cat $HOME/.local/state/theme_state.txt)"
+if [ $state_theme = "Wallpaper" ]; then
+  wallpaper_dir="$HOME/.config/hypr/wallpapers/unthemed/"
+else
+  wallpaper_dir="$HOME/.config/hypr/wallpapers/$state_theme"
+fi
 
 # Get a list of all wallpaper files in the directory
 files=("$wallpaper_dir"/*)
@@ -30,6 +35,16 @@ get_random_wallpaper() {
 # Function to set the wallpaper
 set_wallpaper() {
     local wallpaper="$1"
+    if [ $state_theme = "Wallpaper" ]; then
+      wal --backend haishoku -i $wallpaper
+      $HOME/.config/hypr/script<D-is/reload_theme.sh
+    elif [ $state_theme = "Dracula" ]; then
+      wal --theme dracula
+      $HOME/.config/hypr/scripts/reload_theme.sh
+    #elif [ $state_theme = "Mocha" ]; then
+    fi
+    killall rofi
+
     swww img $wallpaper --transition-type wipe --transition-fps 120
 }
 
