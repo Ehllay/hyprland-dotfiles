87b098b743d0efb9d58debde3b590ffe4c0baa9a
Ehllay <max.pruther@gmail.com>
2023-10-17 17:53:39 +0100

    Fixed rofi theme script not exiting

--- home/.config/hypr/scripts/theme.sh
+++ home/.config/hypr/scripts/theme.sh
@@ -6,20 +6,24 @@ script_dir="$HOME/.config/hypr/scripts"
 if [ x"$@" = x"Dracula" ]; then
   echo "Dracula" > $theme_state 
   $script_dir/wallpaper.sh
-
   $script_dir/applytheme.sh
+
+  killall rofi
   exit 0
 
 elif [ x"$@" = x"Catppuccin Mocha" ]; then
   echo "Mocha" > $theme_state
   $script_dir/wallpaper.sh
   $script_dir/applytheme.sh
-  
+
+  killall rofi
   exit 0
 
 elif [ x"$@" = x"Wallpaper" ]; then
   echo "Wallpaper" > $theme_state
   $script_dir/wallpaper.sh
+
+  killall rofi
   exit 0
 fi
 
