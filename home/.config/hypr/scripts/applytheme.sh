92a1956ee79544efa95f2efdc2c4310a6c3767be
Ehllay <max.pruther@gmail.com>
2023-10-21 19:56:03 +0100

    Added feh config and moved the order of the cursor setter

--- home/.config/hypr/scripts/applytheme.sh
+++ home/.config/hypr/scripts/applytheme.sh
@@ -5,17 +5,20 @@ state_file="$HOME/.local/state/theme_state.txt"
 if grep -q Dracula $state_file; then
   #$script_dir/wallpaper.sh
   wal --theme dracula 
-  hyprctl setcursor Dracula-cursors 24
   gsettings set org.gnome.desktop.interface gtk-theme 'Dracula'
   gsettings set org.gnome.desktop.interface cursor-theme 'Dracula-cursors'
+  hyprctl setcursor Dracula-cursors 24
 
 
 elif grep -q Mocha $state_file; then
   wal --theme catppuccin_mocha
-  hyprctl setcursor Catppuccin-Mocha-Lavender-Cursors 24
   gsettings set org.gnome.desktop.interface gtk-theme 'Catppuccin-Mocha-Standard-Lavender-Dark'
   gsettings set org.gnome.desktop.interface cursor-theme 'Catppuccin-Mocha-Lavender-Cursors'
+  hyprctl setcursor Catppuccin-Mocha-Lavender-Cursors 24
 
+elif grep -q Wallpaper $state_file; then
+  gsettings set org.gnome.desktop.interface cursor-theme 'McMojave-cursors'
+  hyprctl setcursor McMojave-cursors 24
 fi
 
 killall waybar
