0fcf4b360f417a0ebd6515331d427532796fa043
Ehllay <max.pruther@gmail.com>
2023-11-04 12:50:40 +0000

    Pfetch config and fixed waybar

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -3,7 +3,7 @@ set fish_greeting
 if status is-interactive
     # Commands to run in interactive sessions can go here
     cat ~/.cache/wal/sequences
-    pfetch
+    PF_INFO="ascii title os wm kernel uptime pkgs memory" pfetch
 end
 
 starship init fish | source
