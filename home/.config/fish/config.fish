c9a65459c61a379012beab57ac7cae8116b4cf84
Ehllay <max.pruther@gmail.com>
2023-12-08 16:31:13 +0000

    Changed 2nd window border color, better wlogout animation and added wal-set command

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,5 +1,12 @@
 set fish_greeting
 
+function wal-set
+  wal -n -t -i $argv --saturate 0.75 --backend haishoku
+  swww img $argv --transition-type wipe --transition-fps 90
+  ~/.config/hypr/scripts/applytheme.sh
+end
+
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
     cat ~/.cache/wal/sequences
--- home/.config/hypr/scripts/applytheme.sh
+++ home/.config/hypr/scripts/applytheme.sh
@@ -4,21 +4,21 @@ state_file="$HOME/.local/state/theme_state.txt"
 
 if grep -q Dracula $state_file; then
   #$script_dir/wallpaper.sh
-  wal -n --theme dracula 
+  wal -n -t -q --theme dracula 
   hyprctl setcursor Dracula-cursors 24
   gsettings set org.gnome.desktop.interface gtk-theme 'Dracula'
   gsettings set org.gnome.desktop.interface cursor-theme 'Dracula-cursors'
 
 
 elif grep -q Mocha $state_file; then
-  wal -n --theme catppuccin_mocha
+  wal -n -t -q --theme catppuccin_mocha
   hyprctl setcursor Catppuccin-Mocha-Lavender-Cursors 24
   gsettings set org.gnome.desktop.interface gtk-theme 'Catppuccin-Mocha-Standard-Lavender-Dark'
   gsettings set org.gnome.desktop.interface cursor-theme 'Catppuccin-Mocha-Lavender-Cursors'
 
 elif grep -q Wallpaper $state_file; then
-  gsettings set org.gnome.desktop.interface cursor-theme 'McMojave-cursors'
-  hyprctl setcursor McMojave-cursors 24
+  gsettings set org.gnome.desktop.interface cursor-theme 'Bibata'
+  hyprctl setcursor Bibata 24
 fi
 
 killall waybar
