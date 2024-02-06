69f8faf268ebfec3fe9fb043ae4a68a703360e99
Ehllay <max.pruther@gmail.com>
2024-02-06 15:52:12 +0000

    Added fixes config

--- home/.config/dunst/sound.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-pw-play /usr/share/sounds/freedesktop/stereo/message-new-instant.oga
--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,4 +1,13 @@
 set fish_greeting
+set EDITOR nvim
+
+function wal-set
+  wal -n -t -q -i $argv --saturate 0.75 --backend haishoku
+  swww img $argv --transition-type wipe --transition-fps 90
+  ~/.config/hypr/scripts/applytheme.sh
+end
+
+alias ls lsd
 
 
 if status is-interactive
