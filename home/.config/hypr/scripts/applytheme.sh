0169908c660733193c47ffc5169dd30bd878d78b
Ehllay <max.pruther@gmail.com>
2024-03-27 15:53:24 +0000

    Added swappy and removed catppuccin cursors

--- home/.config/hypr/scripts/applytheme.sh
+++ home/.config/hypr/scripts/applytheme.sh
@@ -5,20 +5,15 @@ state_file="$HOME/.local/state/theme_state.txt"
 if grep -q Dracula $state_file; then
   #$script_dir/wallpaper.sh
   wal -n -t -q --theme dracula 
-  hyprctl setcursor Dracula-cursors 24
   gsettings set org.gnome.desktop.interface gtk-theme 'Dracula'
-  gsettings set org.gnome.desktop.interface cursor-theme 'Dracula-cursors'
 
 
 elif grep -q Mocha $state_file; then
   wal -n -t -q --theme catppuccin_mocha
-  hyprctl setcursor Catppuccin-Mocha-Lavender-Cursors 24
   gsettings set org.gnome.desktop.interface gtk-theme 'Catppuccin-Mocha-Standard-Lavender-Dark'
-  gsettings set org.gnome.desktop.interface cursor-theme 'Catppuccin-Mocha-Lavender-Cursors'
 
 elif grep -q Pywal $state_file; then
-  gsettings set org.gnome.desktop.interface cursor-theme 'Bibata'
-  hyprctl setcursor Bibata 24
+  gsettings set org.gnome.desktop.interface gtk-theme 'Catppuccin-Mocha-Standard-Lavender-Dark'
 fi
 
 killall waybar
